4007964bb6f4070741c75cb7c0a94e5bc76865c4
lada crin <golomazdinp@gmail.com>
2024-07-26 08:47:23 +0300

    refactor set tests

--- set_test.go
+++ set_test.go
@@ -1,43 +1,79 @@
 package mypackage_test
 
 import (
-	"slices"
+	"fmt"
 	"testing"
 
 	mypackage "github.com/unbearablelightnessofbeing/my-package"
 )
 
+type SetTestCase[T comparable] struct {
+	Add            []T
+  Has T
+	ShouldHave     bool
+	ExpectedValues []T
+}
+
+func checkSetCases[T comparable](t *testing.T, cases []SetTestCase[T]) {
+  set := mypackage.NewSet[T]()
+
+	for _, testCase := range cases {
+    if len(testCase.Add) != 0 {
+      set.Add(testCase.Add...) 
+    }
+
+    if set.Has(testCase.Has) != testCase.ShouldHave {
+      t.Fatalf("set should have element %v, set: %v", testCase.Has, set)
+    }
+
+    elemsStr := fmt.Sprintf("%v", set.GetElements())
+    expectedStr := fmt.Sprintf("%v", testCase.ExpectedValues)
+    if elemsStr != expectedStr {
+      t.Fatalf("values in set do not match:\nexpected:\n\t%v\ngot:\n\t%v", elemsStr, expectedStr)
+    }
+	}
+}
+
 func TestSet(t *testing.T) {
-  set := mypackage.NewSet[int]()
+	cases := []SetTestCase[int]{
+    {
+      Add: []int{},
+      Has: 1,
+      ShouldHave: false,
+      ExpectedValues: []int{},
+    },
+    {
+      Add: []int{1},
+      Has: 1,
+      ShouldHave: true,
+      ExpectedValues: []int{1},
+    },
+    {
+      Add: []int{2},
+      Has: 2,
+      ShouldHave: true,
+      ExpectedValues: []int{1, 2},
+    },
+    {
+      Add: []int{},
+      Has: 3,
+      ShouldHave: false,
+      ExpectedValues: []int{1, 2},
+    },
+    {
+      Add: []int{3, 4},
+      Has: 3,
+      ShouldHave: true,
+      ExpectedValues: []int{1, 2, 3, 4},
+    },
+	}
 
-  set.Add(1)
-  set.Add(3)
-
-  if !set.Has(1) {
-    t.Fatalf("set is expected to have value '1'")
-  }
-  if !set.Has(3) {
-    t.Fatalf("set is expected to have value '3'")
-  }
-
-  if !slices.Equal(set.GetElements(), []int{1,3}) {
-    t.Fatalf("set is expected to have values '{1,3}'")
-  }
-
-  set.Add(4, 5)
-  if !slices.Equal(set.GetElements(), []int{1,3,4,5}) {
-    t.Fatalf("set is expected to have values '{1,3,4,5}'\n got: %v", set.GetElements())
-  }
-
-  set.Add(5,5,5)
-  if !slices.Equal(set.GetElements(), []int{1,3,4,5}) {
-    t.Fatalf("set is expected to have values '{1,3,4,5}'")
-  }
+  checkSetCases(t, cases)
 }
 
 // func TestAsyncSet(t *testing.T) {
 //   set := mypackage.NewSet[string]()
-// 
+//
 //   go func() {
 //     set.Add("test_1")
 //     set.Add("test_2")
